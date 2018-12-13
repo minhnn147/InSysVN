3372511b618864a96a9774af67a658c0b233104d
thongnh <thongnh@phucthanhgroup.com>
2018-12-13 17:42:53 +0700

    fix bug

--- InSysVN/InSys/local/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InSys.MvcApplication" Language="C#" %>
---
+++ InSysVN/InsysVina/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Framework.EF;
+using LIB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsysVina.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICategory category;
+        public HomeController()
+        {
+            category = SingletonIpl.GetInstance<IplCategory>();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Category = category.GetAllData().Where(x => x.isActive == true).ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
